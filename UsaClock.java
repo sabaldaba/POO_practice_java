2555b9f10a9eb4ccf168741feb0bfa9105020bea
sabaldaba <jor.sabal@hotmail.com>
2016-02-09 15:37:26 -0600

    Create UsaClock.java

---
+++ UsaClock.java
@@ -0,0 +1,28 @@
+public class UsaClock{
+	public static void main(String[]args){
+		Clock gdl = new Clock();
+		Clock tij = new Clock();//(10,27,5);
+		Clock LA = new Clock();//(10,28,6);
+
+		gdl.printTime();
+
+		gdl.getHours();
+		gdl.printTime();
+		/*tij.printTime();
+
+		gdl.setTime(12,33,9);
+		gdl.incrementSeconds();
+
+		tij.setTime(30,33,9);
+		tij.incrementMinutes();
+
+		LA.incrementHours();
+
+		gdl.printTime();
+		tij.printTime();
+		LA.printTime();*/
+		
+		
+}
+
+}
