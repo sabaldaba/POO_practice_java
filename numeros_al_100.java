f140d84ece4039492d05f68244a00b9e3875415a
sabaldaba <jor.sabal@hotmail.com>
2016-02-09 15:34:19 -0600

    Create numeros_al_100.java

---
+++ numeros_al_100.java
@@ -0,0 +1,9 @@
+/*Programa que imprime del 1 al 100 */
+class numeros_al_100{
+  public static void main(String[]args) {
+
+    for (byte i = 1; i < 101; i++){
+      System.out.println(i);
+    }
+  }
+}
