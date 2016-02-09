b40ae7976d89bf0f726cdb415a9b62b78e92096b
sabaldaba <jor.sabal@hotmail.com>
2016-02-09 15:30:10 -0600

    Create grades.java

---
+++ grades.java
@@ -0,0 +1,15 @@
+/*Programa que lee una calificacion para decir si es aprobatoria o no*/
+class califi  {
+  public static void main(String[] args)  {
+
+  byte cali = 70;
+
+  if (cali >= 70){
+    System.out.println("Aprobado");
+  }
+  else{
+    System.out.println("Reprobado");
+  }
+  }
+
+}
