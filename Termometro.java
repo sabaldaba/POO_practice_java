adb7cc8762c76d88ae81754aab50182cb8a009f4
sabaldaba <jor.sabal@hotmail.com>
2016-02-09 15:36:39 -0600

    Create Termometro.java

---
+++ Termometro.java
@@ -0,0 +1,21 @@
+public class Termometro{
+	private int temp;
+
+	public Termometro(){
+		temp = 25;
+	}
+
+	public Termometro(int t){
+		temp= t;	
+
+	}
+	
+	public void setTemp(int t){
+		temp = t;
+	}
+
+	public int getTemp(){
+		return temp;
+	}
+
+}
