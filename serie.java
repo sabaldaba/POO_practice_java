b091e7f738692a6a625382d85a9b2f0393a61737
sabaldaba <jor.sabal@hotmail.com>
2016-02-09 15:35:10 -0600

    Create serie.java

---
+++ serie.java
@@ -0,0 +1,13 @@
+/*Programa que recibe un numero y despues hace una serie con el*/
+
+class serie {
+  public static void main(String[]args){
+
+  int num = 23;
+  int res = 0;
+  for (byte i = 0; i < 50; i++){
+    res = res+num;
+    System.out.println(res);
+  }
+  }
+}
